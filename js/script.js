796d92fc2838c66a517084fe14b1453964a1154d
mirzoo <mirzo.okhunov@gmail.com>
2022-08-03 12:16:57 +0300

    add loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+$(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+      $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
+
+
 $(document).ready(function() {
 
     $('.header-burger').click(function(event) {
